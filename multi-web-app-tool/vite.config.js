5c70ec0943db2b7b4a68c367966fb0515031df7e
Rob Dunn <dunnrob@hotmail.co.uk>
2024-03-17 20:59:02 +0000

    edit to config

--- multi-web-app-tool/vite.config.js
+++ multi-web-app-tool/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/work-projects-lbg/' // Adjust this to match your GitHub Pages repository name
+  base: '/work-projects-lbg/multi-web-app-tool/'
 });
