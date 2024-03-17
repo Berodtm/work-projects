2816ae3f863f990647d8f42fc6a83031a8250cd1
Rob Dunn <dunnrob@hotmail.co.uk>
2024-03-17 20:40:51 +0000

    Updated vite.config.js for GitHub Pages deployment

--- multi-web-app-tool/vite.config.js
+++ multi-web-app-tool/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/work-projects-lbg/' // Adjust this to match your GitHub Pages repository name
+});
