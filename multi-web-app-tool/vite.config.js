0fcc362698c7e9147cfd350ee35d0eb2026e3362
Rob Dunn <dunnrob@hotmail.co.uk>
2024-03-18 08:29:40 +0000

    Update vite.config.js

--- multi-web-app-tool/vite.config.js
+++ multi-web-app-tool/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/work-projects-lbg/multi-web-app-tool/',
   plugins: [react()],
-  base: '/work-projects-lbg/multi-web-app-tool/'
 });
