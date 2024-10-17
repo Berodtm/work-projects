1844a2197a59eeb19e7f3ce16a766d5387bc8796
Rob Dunn <dunnrob@hotmail.co.uk>
2024-10-17 14:28:22 +0100

    added code to match work a/c

--- ops-aem-id-gen/javascript/autoUniqueId.js
+++ ops-aem-id-gen/javascript/autoUniqueId.js
@@ -1,9 +1,24 @@
+document.getElementById("appRd").addEventListener("change", generateAutoUID)
+document.getElementById("device-type-dkp").addEventListener("change", generateAutoUID)
+document.getElementById("device-type-app").addEventListener("change", generateAutoUID)
+document.getElementById("device-type-mbr").addEventListener("change", generateAutoUID)
+document.getElementById("device-type-cwa").addEventListener("change", generateAutoUID)
+
 function generateAutoUID() {
     const uniqueIDInput = document.getElementById("unique-id");
     const campaignNameInput = document.getElementById("campaign-name");
     const versionInput = document.getElementById("version");
-
+    const appRedesignCheckBox = document.getElementById("appRd")
+    const deviceTypeDkp = document.getElementById("device-type-dkp")
+    const deviceTypeMbr = document.getElementById("device-type-mbr")
+    const deviceTypeCwa = document.getElementById("device-type-cwa")
     let campaignName = campaignNameInput.value.replace(/\s/g, "-");
+
+    if (!appRedesignCheckBox.checked || appRedesignCheckBox.checked && deviceTypeDkp.checked || appRedesignCheckBox.checked && deviceTypeMbr.checked || appRedesignCheckBox.checked && deviceTypeCwa.checked) {
+        uniqueIDInput.maxLength = 9;
+    } else {
+        uniqueIDInput.maxLength = 7;
+    }
 
     // Replace "lp" with "l0"
     campaignName = campaignName.replace(/lp/g, "l0");
@@ -35,10 +50,14 @@ function generateAutoUID() {
     const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
     const year = currentDate.getFullYear().toString().slice(-2);
 
-    if (campaignName.trim().length > 0) {
+    if (campaignName.trim().length > 0 && (!appRedesignCheckBox.checked || (appRedesignCheckBox.checked && (deviceTypeDkp.checked || deviceTypeMbr.checked || deviceTypeCwa.checked)))) {
         const campaignPrefix = campaignName.substring(0, 5).padEnd(5, "0");
+        uniqueIDInput.value = `${month}${year}${campaignPrefix}`;
+    } else if (appRedesignCheckBox.checked) {
+        const campaignPrefix = campaignName.substring(0, 3).padEnd(3, "0");
         uniqueIDInput.value = `${month}${year}${campaignPrefix}`;
     } else {
         uniqueIDInput.value = "";
     }
+    
 }
--- ops-aem-id-gen/javascript/generateAemIds.js
+++ ops-aem-id-gen/javascript/generateAemIds.js
@@ -1,76 +1,140 @@
 function generateAEMIDs() {
-    // Function to get checked values from a group of checkboxes
-    function getCheckedValues(containerId) {
-      const checkboxes = document.querySelectorAll(
-        `#${containerId} input[type='checkbox']:checked`
-      );
-      return Array.from(checkboxes).map((checkbox) => checkbox.value);
-    }
-    // Getting values from other fields
-    const productTypeSelect = document.getElementById("product-type").value;
-    const placementTypeSelect =
-      document.getElementById("placement-type").value;
-    const tokensSelect = document.getElementById("tokens").value;
-    const ratedSelect = document.getElementById("rated").value;
-    const purposeSelect = document.getElementById("purpose").value;
-    const subProductInput = document.getElementById("sub-product").value.toLowerCase(); // added .toLowerCase()
-    const campaignNameInput =
-      document.getElementById("campaign-name").value.toLowerCase(); // added .toLowerCase()
-    const uniqueIDInput = document.getElementById("unique-id").value.toLowerCase(); // added .toLowerCase()
-    const versionInput = document.getElementById("version").value;
-    // Getting selected brands, asset types, and device types
-    const selectedBrands = getCheckedValues("brand-checkboxes");
-    const selectedAssetTypes = getCheckedValues("asset-type-checkboxes");
-    const selectedDeviceTypes = getCheckedValues("device-type");  
-    // Check if all fields are filled in and at least one checkbox is checked in each group
-    if (
-      !productTypeSelect ||
-      !placementTypeSelect ||
-      !tokensSelect ||
-      !ratedSelect ||
-      !purposeSelect ||
-      !subProductInput ||
-      !campaignNameInput ||
-      !uniqueIDInput ||
-      selectedBrands.length === 0 ||
-      selectedAssetTypes.length === 0 ||
-      selectedDeviceTypes.length === 0
-    ) {
-      alert("Please fill in all fields and select at least one checkbox in each group.");
-      return;
-    }
+  function getCheckedValues(containerId) {
+    const checkboxes = document.querySelectorAll(
+      `#${containerId} input[type='checkbox']:checked`
+    );
+    return Array.from(checkboxes).map((checkbox) => checkbox.value);
+  }
   
-    // Clear previous IDs
-    generatedIDArray = [];
-    // Iterate over each selected brand, asset type, and device type
-    selectedBrands.forEach((brand) => {
-      selectedAssetTypes.forEach((assetType) => {
-        selectedDeviceTypes.forEach((deviceType) => {
-          if (deviceType === "mbr" && !["bpt", "asm"].includes(assetType) || deviceType === "app" && ["stk", "til"].includes(assetType) || deviceType === "dkp" && ["lob", "mrc", "nvt", "phl"].includes(assetType)) {
-            return;
-          }
+  const productTypeSelect = document.getElementById("product-type").value;
+  const placementTypeSelect = document.getElementById("placement-type").value;
+  const tokensSelect = document.getElementById("tokens").value;
+  const ratedSelect = document.getElementById("rated").value;
+  const purposeSelect = document.getElementById("purpose").value;
+  const subProductInput = document
+    .getElementById("sub-product")
+    .value.toLowerCase(); 
+  const campaignNameInput = document
+    .getElementById("campaign-name")
+    .value.toLowerCase(); 
+  const uniqueIDInput = document
+    .getElementById("unique-id")
+    .value.toLowerCase(); 
+  const versionInput = document.getElementById("version").value;
+  
+  const selectedBrands = getCheckedValues("brand-checkboxes");
+  const selectedAssetTypes = getCheckedValues("asset-type-checkboxes");
+  const selectedDeviceTypes = getCheckedValues("device-type");
+  // Check if all fields are filled in and at least one checkbox is checked in each group
+  if (
+    !productTypeSelect ||
+    !placementTypeSelect ||
+    !tokensSelect ||
+    !ratedSelect ||
+    !purposeSelect ||
+    !subProductInput ||
+    !campaignNameInput ||
+    !uniqueIDInput ||
+    selectedBrands.length === 0 ||
+    selectedAssetTypes.length === 0 ||
+    selectedDeviceTypes.length === 0
+  ) {
+    alert(
+      "Please fill in all fields and select at least one checkbox in each group."
+    );
+    return;
+  }
+
+  // Clear previous IDs - commented this line out as I dont want to clear IDs but leaving it here in case a bug gets introduced. Will clean later. 
+  // generatedIDArray = [];
+  const appRedesignCheckBox = document.getElementById("appRd").checked
+  const arAssetTypes = new Set(["afw", "gam", "mam", "mmt", "sfw", "vpd", "aev"]);
+  
+  // Iterate over each selected brand, asset type, and device type
+  selectedBrands.forEach((brand) => {
+    selectedAssetTypes.forEach((assetType) => {
+      selectedDeviceTypes.forEach((deviceType) => {
+        if (
+          (deviceType === "mbr" &&
+            !["bpt", "asm", "stk", "dpp", "fea"].includes(assetType)) ||
+          (deviceType === "app" &&
+            ["stk", "til", "fea", "vpd"].includes(assetType)) ||
+          (deviceType === "dkp" &&
+            [
+              "mmt",
+              "lob",
+              "mrc",
+              "nvt",
+              "phl",
+              "afw",
+              "aev",
+              "gam",
+              "mam",
+              "sfw",
+              "pom",
+              "vpd",
+            ].includes(assetType)) ||
+          (deviceType === "cwa" && !["vpd"].includes(assetType))
+        ) {
+          return;
+        }
+        const appRedesignCheckBox = document.getElementById("appRd");
+        if (!appRedesignCheckBox.checked || (deviceType === "dkp" || deviceType === "mbr" || deviceType === "cwa")) {
           for (let i = 1; i <= versionInput; i++) {
             // Generate ID for each brand, asset type, and device type combination
             const bosdkpbpt =
-              brand.substring(0, 3) +
-              deviceType.substring(0, 3) +
-              assetType;
+              brand.substring(0, 3) + deviceType.substring(0, 3) + assetType;
             const id = `core-leads/${brand}/${productTypeSelect}/${deviceType}/${assetType}/${placementTypeSelect}/${tokensSelect}/${ratedSelect}/${purposeSelect}/${bosdkpbpt}-${subProductInput.replace(
               /\s/g,
               "-"
-            )}-${campaignNameInput.replace(
+            )}-${campaignNameInput.replace(/\s/g, "-")}-${uniqueIDInput.replace(
               /\s/g,
               "-"
-            )}-${uniqueIDInput.replace(/\s/g, "-")}${i}`;
+            )}${i}`;
             // Check if ID already exists
             if (!generatedIDArray.includes(id)) {
               generatedIDArray.push(id); // Add ID to array
             }
           }
-        });
+        } else if (appRedesignCheckBox.checked) {
+          for (let i = 1; i <= versionInput; i++) {
+            // Generate ID for each brand, asset type, and device type combination
+            const bosdkpbpt =
+              brand.substring(0, 3) + deviceType.substring(0, 3) + assetType;
+            let id = `core-leads/${brand}/${productTypeSelect}/${deviceType}/${assetType}/${placementTypeSelect}/${tokensSelect}/${ratedSelect}/${purposeSelect}/${bosdkpbpt}-${subProductInput.replace(
+              /\s/g,
+              "-"
+            )}-${campaignNameInput.replace(/\s/g, "-")}-${uniqueIDInput.replace(
+              /\s/g,
+              "-"
+            )}${i}ar`;
+            // multi device and app redesign scenario 
+            if (selectedDeviceTypes.length > 1) {
+              const trimUniqueIDInput = uniqueIDInput.slice(0, -2); // Adjust the number of characters to trim as needed
+              id = `core-leads/${brand}/${productTypeSelect}/${deviceType}/${assetType}/${placementTypeSelect}/${tokensSelect}/${ratedSelect}/${purposeSelect}/${bosdkpbpt}-${subProductInput.replace(
+                /\s/g,
+                "-"
+              )}-${campaignNameInput.replace(/\s/g, "-")}-${trimUniqueIDInput}${i}ar`;
+            } else {
+              id = `core-leads/${brand}/${productTypeSelect}/${deviceType}/${assetType}/${placementTypeSelect}/${tokensSelect}/${ratedSelect}/${purposeSelect}/${bosdkpbpt}-${subProductInput.replace(
+                /\s/g,
+                "-"
+              )}-${campaignNameInput.replace(/\s/g, "-")}-${uniqueIDInput.replace(
+                /\s/g,
+                "-"
+              )}${i}ar`;
+            }
+            
+            // Check if ID already exists
+            if (!generatedIDArray.includes(id)) {
+              generatedIDArray.push(id); // Add ID to array
+            }
+          }
+        }
       });
     });
-    // Update the textarea with the generated IDs
-    const generatedIDsTextArea = document.getElementById("generated-ids");
-    generatedIDsTextArea.value = generatedIDArray.join("\n");
-  }
+  });
+  // Update the textarea with the generated IDs
+  const generatedIDsTextArea = document.getElementById("generated-ids");
+  generatedIDsTextArea.value = generatedIDArray.join("\n");
+}
